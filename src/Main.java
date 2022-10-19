8fdc85c992a1be5f0f6af47a8105cdf66f2ae692
Ilia <illigiorno@gmail.com>
2022-10-20 01:09:40 +0400

    Final

--- src/Main.java
+++ src/Main.java
@@ -7,20 +7,20 @@ public class Main {
 
         double left = 0;
         double right = 4;
-        double[] val = { 1, 4 , 9 , 16};
-        TabulatedFunction fir = new TabulatedFunction(left, right, val);
+        double[] values = { 1, 4 , 9 , 16};
+        TabulatedFunction arr = new TabulatedFunction(left, right, values);
 
 
-        fir.print();
-        fir.deletePoint(1);
+        arr.print();
+        arr.deletePoint(1);
         FunctionPoint n = new FunctionPoint(5,25);
-        fir.addPoint(n);
+        arr.addPoint(n);
         System.out.println();
-        fir.print();
+        arr.print();
 
-        //System.out.println(fir.getFunctionValue(4.0));
+        //System.out.println(arr.getFunctionValue(4.0));
 
-        //FunctionPoint a = fir.getPoint(0);
+        //FunctionPoint a = arr.getPoint(0);
         //System.out.println(a.getX());
 
     }
